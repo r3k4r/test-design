c94a79995c99ea84df0b05d2dc6cf553845574cc
Rekar Jamal <rekarjamal33@gmail.com>
2025-12-02 14:49:40 +0300

    Update CircleSection.vue

--- src/components/LandingPage/CircleSection.vue
+++ src/components/LandingPage/CircleSection.vue
@@ -1,33 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from '../ui/Button.vue'
+</script>
 
 <template>
   <section
-    class="mt-[125px] md:mt-[343px] max-w-[1102px] w-full padding relative h-full min-h-[946px]"
+    class="mt-[125px] md:mt-[343px] max-w-[1102px] w-full padding relative h-full min-h-[946px] flex items-center justify-center"
   >
-    hh
     <div
-      class="w-[250px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] -z-[1]"
+      class="w-[250px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
     <div
-      class="w-[100px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] -z-[1]"
+      class="w-[100px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
     <div
-      class="w-[375px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] -z-[1]"
+      class="w-[375px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
     <div
-      class="w-[550px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] -z-[1]"
+      class="w-[550px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
     <div
-      class="w-[725px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] -z-[1]"
+      class="w-[725px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
+
+    <div
+      class="w-[350px] aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] bg-primary/5 blur-[30px]"
+    />
+
+    <div class="relative flex flex-col items-center justify-center">
+      <h1>text</h1>
+      <Button class="rounded-full! px-8">Learn more</Button>
+    </div>
   </section>
 </template>
-
-<style scoped>
-.absolute-centered {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-</style>
