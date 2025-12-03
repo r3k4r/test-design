3f51bcf6a916c421aa57f54e5519b8bea75994a5
Rekar Jamal <rekarjamal33@gmail.com>
2025-12-03 10:06:56 +0300

    Update CircleSection.vue

--- src/components/LandingPage/CircleSection.vue
+++ src/components/LandingPage/CircleSection.vue
@@ -7,19 +7,19 @@ import Button from '../ui/Button.vue'
     class="mt-[125px] md:mt-[343px] max-w-[1102px] w-full padding relative h-full min-h-[946px] flex items-center justify-center"
   >
     <div
-      class="w-[250px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
+      class="w-[250px] border border-gray-400/20 bg-transparent aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
     <div
-      class="w-[100px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
+      class="w-[100px] border border-gray-400/20 bg-transparent aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
     <div
-      class="w-[375px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
+      class="w-[375px] border border-gray-400/20 bg-transparent aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
     <div
-      class="w-[550px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
+      class="w-[550px] border border-gray-400/20 bg-transparent aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
     <div
-      class="w-[725px] border border-gray-400/20 aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
+      class="w-[725px] border border-gray-400/20 bg-transparent aspect-square rounded-full absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
     />
 
     <div
