41101a4e08349d99f298ddfb248f1ec40231cb78
Rekar Jamal <rekarjamal33@gmail.com>
2025-12-04 12:36:53 +0300

    Update Footer.vue

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -47,91 +47,97 @@ const year = new Date().getFullYear()
 
         <!-- Body -->
         <div class="w-full flex flex-col lg:flex-row items-start justify-between gap-8 lg:gap-0">
-          <!-- Left Section - Contact Info -->
-          <div class="w-full lg:w-1/2 flex flex-col gap-4">
-            <a href="mailto:info@babylonholiday.com" class="flex items-center gap-3 w-fit">
-              <Mail :size="20" class="text-black shrink-0" />
-              <span class="text-sm font-semibold underline text-black"
-                >info@babylonholiday.com</span
-              >
-            </a>
-            <a href="tel:++9647735566009" class="flex items-center gap-3 w-fit">
-              <Phone :size="20" class="text-black shrink-0" />
-              <span class="text-sm font-semibold underline text-black">+964 773 556 6009</span>
-            </a>
+          <div class="flex flex-col sm:flex-row gap-[72px] w-full items-start justify-between">
+            <!-- Left Section - Contact Info -->
+            <div class="w-full flex flex-col gap-4">
+              <a href="mailto:info@babylonholiday.com" class="flex items-center gap-3 w-fit">
+                <Mail :size="20" class="text-black shrink-0" />
+                <span class="text-sm font-semibold underline text-black"
+                  >info@babylonholiday.com</span
+                >
+              </a>
+              <a href="tel:++9647735566009" class="flex items-center gap-3 w-fit">
+                <Phone :size="20" class="text-black shrink-0" />
+                <span class="text-sm font-semibold underline text-black">+964 773 556 6009</span>
+              </a>
+            </div>
+
+            <!-- Right Section - Menu, Visit Us -->
+            <div
+              class="w-full flex items-start justify-start sm:justify-end lg:mr-[72px] gap-[72px]"
+            >
+              <!-- Menu -->
+              <div class="flex flex-col gap-4">
+                <h3 class="text-base sm:text-lg font-semibold text-black">Menu</h3>
+                <ul class="flex flex-col gap-3">
+                  <li>
+                    <router-link to="/about" class="text-sm font-semibold text-[#757575]"
+                      >About Us</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link to="/contact" class="text-sm font-semibold text-[#757575]"
+                      >Contact Us</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link to="/faqs" class="text-sm font-semibold text-[#757575]"
+                      >FAQs</router-link
+                    >
+                  </li>
+                </ul>
+              </div>
+
+              <!-- Visit Us -->
+              <div class="flex flex-col gap-4">
+                <h3 class="text-base sm:text-lg font-semibold text-black">Visit Us</h3>
+                <ul class="flex flex-col gap-3">
+                  <li>
+                    <router-link
+                      to="/iraq?country=iraq"
+                      class="text-sm font-semibold text-[#757575]"
+                      >Iraq</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/germany?country=germany"
+                      class="text-sm font-semibold text-[#757575]"
+                      >Germany</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link to="/uae?country=uae" class="text-sm font-semibold text-[#757575]"
+                      >UAE</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/turkey?country=turkey"
+                      class="text-sm font-semibold text-[#757575]"
+                      >Turkey</router-link
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
 
-          <!-- Right Section - Menu, Visit Us, Newsletter -->
-          <div class="w-full lg:w-1/2 flex items-start justify-end gap-[72px]">
-            <!-- Menu -->
+          <!-- Newsletter -->
+          <div class="flex flex-col gap-8 sm:col-span-2 lg:col-span-1">
             <div class="flex flex-col gap-4">
-              <h3 class="text-base sm:text-lg font-semibold text-black">Menu</h3>
-              <ul class="flex flex-col gap-3">
-                <li>
-                  <router-link to="/about" class="text-sm font-semibold text-[#757575]"
-                    >About Us</router-link
-                  >
-                </li>
-                <li>
-                  <router-link to="/contact" class="text-sm font-semibold text-[#757575]"
-                    >Contact Us</router-link
-                  >
-                </li>
-                <li>
-                  <router-link to="/faqs" class="text-sm font-semibold text-[#757575]"
-                    >FAQs</router-link
-                  >
-                </li>
-              </ul>
+              <h3 class="text-base sm:text-lg font-semibold text-black">Newsletter</h3>
+              <p class="text-sm font-semibold text-[#757575] max-w-[352px]">
+                Subscribe with our newsletter to receive latest updates
+              </p>
             </div>
-
-            <!-- Visit Us -->
-            <div class="flex flex-col gap-4">
-              <h3 class="text-base sm:text-lg font-semibold text-black">Visit Us</h3>
-              <ul class="flex flex-col gap-3">
-                <li>
-                  <router-link to="/iraq?country=iraq" class="text-sm font-semibold text-[#757575]"
-                    >Iraq</router-link
-                  >
-                </li>
-                <li>
-                  <router-link
-                    to="/germany?country=germany"
-                    class="text-sm font-semibold text-[#757575]"
-                    >Germany</router-link
-                  >
-                </li>
-                <li>
-                  <router-link to="/uae?country=uae" class="text-sm font-semibold text-[#757575]"
-                    >UAE</router-link
-                  >
-                </li>
-                <li>
-                  <router-link
-                    to="/turkey?country=turkey"
-                    class="text-sm font-semibold text-[#757575]"
-                    >Turkey</router-link
-                  >
-                </li>
-              </ul>
-            </div>
-
-            <!-- Newsletter -->
-            <div class="flex flex-col gap-8 sm:col-span-2 lg:col-span-1">
-              <div class="flex flex-col gap-4">
-                <h3 class="text-base sm:text-lg font-semibold text-black">Newsletter</h3>
-                <p class="text-sm font-semibold text-[#757575] max-w-[352px]">
-                  Subscribe with our newsletter to receive latest updates
-                </p>
-              </div>
-              <div class="flex flex-col sm:flex-row gap-4 w-full">
-                <input
-                  type="email"
-                  placeholder="Enter your email"
-                  class="flex-1 px-5 py-4 sm:py-2.5 text-sm sm:text-base border border-[#D9D9D9] rounded-lg focus:outline-none"
-                />
-                <Button class="border border-black"> Subscribe </Button>
-              </div>
+            <div class="flex flex-row gap-4 w-full">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                class="flex-1 px-5 py-2.5 text-sm sm:text-base border border-[#D9D9D9] rounded-lg focus:outline-none"
+              />
+              <Button class="border border-black"> Subscribe </Button>
             </div>
           </div>
         </div>
@@ -140,9 +146,7 @@ const year = new Date().getFullYear()
       <!-- Divider & Copyright -->
       <div class="mt-[58px] flex flex-col gap-6 pb-8">
         <div class="w-full h-px bg-gray-300"></div>
-        <div
-          class="w-full flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4"
-        >
+        <div class="w-full flex items-start sm:items-center justify-between gap-4">
           <p class="text-sm font-semibold text-[#757575]">
             Copyright {{ year }} All rights reserved
           </p>
